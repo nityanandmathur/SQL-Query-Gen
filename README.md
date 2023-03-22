82f4a9185c1da1a9a15008190f0dc19cf4372b07
nityanandmathur <nityanandmathur@gmail.com>
2023-03-22 10:54:32 +0530

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# SQL Query Generator
+
+## Instructions
+
+1. Create a virtual environment and activate it(according to your OS).
+```bash
+    python -m venv gui
+    source gui/bin/activate #Bash
+    gui\Scripts\activate.bat #CMD
+    gui\Scripts\Activate.ps1 #PowerShell
+```
+
+2. Install __streamlit__.
+```bash
+    pip install streamlit
+```
+
+3. Run __app.py__.
+```bash
+    streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import sqlite3
+import re
+
+conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+def find_query(sentence):
+    select_regex = r"give me the (.+) of the students"
+    update_regex = r"update the (.+) of the student having roll no (\d+) to (\d+)"
+    conditional_regex = r"show the (.+) of the students whose (.+)"
+    all_regex = r"give me all the (.+) of the students"
+
+    select_match = re.search(select_regex, sentence)
+    update_match = re.search(update_regex, sentence)
+    conditional_match = re.search(conditional_regex, sentence)
+    all_match = re.search(all_regex, sentence)
+    keywords = {
+        'more than': '>',
+        'less than': '<',
+        'equal to': '=', 
+        'and':','
+    }
+
+    if select_match:
+        select = select_match.group(1)
+        if select == "roll numbers":
+            sql_query = "SELECT roll FROM students;"
+        elif select == "all the information":
+            sql_query = "SELECT * FROM students;"
+        else:
+            sql_query = f"SELECT {select} FROM students;"
+    elif update_match:
+        col = update_match.group(1)
+        roll_no = update_match.group(2)
+        value = update_match.group(3)
+        sql_query = f"UPDATE students SET {col}={value} WHERE roll={roll_no};"
+    elif conditional_match:
+        select = conditional_match.group(1)
+        condition = conditional_match.group(2)
+        for x in keywords:
+            if x in select: 
+                select=select.replace(x,keywords[x])
+            if x in condition: 
+                condition=condition.replace(x,keywords[x])
+        
+        sql_query = f"SELECT {select} FROM students WHERE {condition};"
+    elif all_match:
+        sql_query = "SELECT * FROM students;"
+    else:
+        sql_query = "Invalid input."
+
+    return sql_query
+
+def execute_query(query):
+    cursor.execute(query)
+    db = cursor.fetchall()
+    return db
+
+st.title('SQL Query Generator')
+text = st.text_input("Text to parse")
+
+if st.button('Find'):
+    result = find_query(text)
+    db = execute_query(result)
+    st.markdown('## Results')
+    st.markdown('Query: ')
+    st.write(result)
+    st.markdown('Output from database: ')
+    st.write(db)
